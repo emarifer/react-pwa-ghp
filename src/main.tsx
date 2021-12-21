9ee72a0720fa0b3ba1ec3af21345dff9245b79f3
emarinfer <enriquemarin_sierra@hotmail.com>
2021-12-21 16:48:07 +0100

    added comments

--- src/main.tsx
+++ src/main.tsx
@@ -12,6 +12,13 @@ ReactDOM.render(
 );
 
 /**
+ * SOLICITAR ACTUALIZACIÓN DE CONTENIDO NUEVO:
+ * https://github.com/antfu/vite-plugin-pwa/issues/156#issuecomment-970165872
+ * https://vite-plugin-pwa.netlify.app/guide/prompt-for-update.html
+ *
+ * Errores IDE 'No se puede encontrar el módulo' (ts2307). CONFIGURACION RECOMENDABLE:
+ * https://vite-plugin-pwa.netlify.app/guide/faq.html#ide-errors-cannot-find-module-ts2307
+ *
  * PAR EL ERROR DE TYPES SE BUSCO «CANNOT FIND MODULE 'VIRTUAL:PWA-REGISTER' OR ITS CORRESPONDING TYPE DECLARATIONS».
  * PROPORCIONAR TIPOS PARA LAS FUNCIONES DEL CLIENTE DEL MÓDULO 'virtual:pwa-register':
  * https://githubmemory.com/repo/antfu/vite-plugin-pwa/issues/40
@@ -21,6 +28,12 @@ ReactDOM.render(
  * GIT CLONE BRANCH: CÓMO CLONAR UNA RAMA ESPECÍFICA:
  * https://www.freecodecamp.org/news/git-clone-branch-how-to-clone-a-specific-branch/
  *
- * SOLICITAR ACTUALIZACIÓN DE CONTENIDO NUEVO:
- * https://vite-plugin-pwa.netlify.app/guide/prompt-for-update.html *
+ * Git Delete Branch – How to Remove a Local or Remote Branch:
+ * https://www.freecodecamp.org/news/git-delete-branch-how-to-remove-a-local-or-remote-branch/
+ *
+ * Bash: duplicar + renombrar carpeta:
+ * https://stackoverflow.com/questions/33343840/bash-duplicate-rename-folder#33366590
+ *
+ * SOBRE EL USO DE LA BIBLIOTECA SWEETALERT:
+ * https://sweetalert2.github.io/
  */
